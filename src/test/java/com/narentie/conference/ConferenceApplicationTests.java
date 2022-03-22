43fd23e2c63cae4eca8b71bb644c4e3cf6b162c2
narentie <narentie.freedom@gmail.com>
2022-03-22 09:01:19 +0200

    First commit

---
+++ src/test/java/com/narentie/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narentie.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
