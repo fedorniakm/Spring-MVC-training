8f877b4a84eeaa6c43fc1f476386aa2717a611f5
narentie <narentie.freedom@gmail.com>
2022-03-22 16:59:09 +0200

    Define ViewResolver

---
+++ src/main/java/com/narentie/conference/ConferenceConfiguration.java
@@ -0,0 +1,19 @@
+package com.narentie.conference;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ConferenceConfiguration {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        var bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/jsp/");
+        bean.setSuffix(".jsp");
+        bean.setOrder(0);
+        return bean;
+    }
+}
