c4fcef5e83ca663162b65796b9792bee5771e3b2
narentie <narentie.freedom@gmail.com>
2022-03-22 17:31:02 +0200

    Add Resource Handler for pdf

--- src/main/java/com/narentie/conference/ConferenceConfiguration.java
+++ src/main/java/com/narentie/conference/ConferenceConfiguration.java
@@ -3,10 +3,18 @@ package com.narentie.conference;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class ConferenceConfiguration {
+public class ConferenceConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf/");
+    }
 
     @Bean
     public ViewResolver viewResolver() {
